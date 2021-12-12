d13491facd4abf8195f0fa4a2b04b0c662ecab7c
hwandongjoo <32835661+hwandongjoo@users.noreply.github.com>
2021-12-12 17:07:20 +0900

    fix update py for only algorithm directory

--- update.py
+++ update.py
@@ -3,50 +3,48 @@
 import os
 from urllib import parse
 
-HEADER="""# 
-# generate_readme_automatically
+HEADER = """ 
+## 알고리즘
 
 ---
+"""
+FOOTER = """
+## 커밋 규칙
+
+---
+
+파일명 : 사이트_난이도_문제이름
+
+커밋명 : 날짜 | 이름
+
+해당날짜 해당 폴더에 올려주세용
+
 """
 
 def main():
     content = ""
     content += HEADER
-    
-    directories = [];
 
-    for root, dirs, files in os.walk("."):
+    directories = []
+
+    for root, dirs, files in os.walk("./algorithm"):
         dirs.sort()
-        if root == '.':
-            for dir in ('.git', '.github'):
-                try:
-                    dirs.remove(dir)
-                except ValueError:
-                    pass
-            continue
-
         category = os.path.basename(root)
-        
-        if category == 'images':
-            continue
-        
         directory = os.path.basename(os.path.dirname(root))
-        
+
         if directory == '.':
-            if len(files) == 1:
-                content += "### [{}]({})\n".format(category, parse.quote(os.path.join(root, files[0])))
-                directories.append(category)
+            # if len(files) == 1:
+            #     content += "## [{}]({})\n".format(category, parse.quote(os.path.join(root, files[0])))
+            #     directories.append(category)
             continue
-            
-        if directory not in directories:
-            content += "### {}\n".format(directory)
-            directories.append(directory)
 
+        content += "### [{}]({})\n".format(category, parse.quote(os.path.join(root, category)))
         for file in files:
-            content += "- [{}]({})\n".format(category, parse.quote(os.path.join(root, file)))
+            content += "- [{}]({})\n".format(file, parse.quote(os.path.join(root, file)))
         content += "\n"
+    content += FOOTER
 
-    with open("README.md", "w") as fd:
+    with open("./algorithm/check.md", "w") as fd:
         fd.write(content)
 
 
